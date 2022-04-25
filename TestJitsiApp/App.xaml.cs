b18c99bd7ad13c122a9bfe707c0e2f90f70adac4
Genaro <genaro.salas@tecsup.edu.pe>
2022-04-25 11:13:36 -0500

    esta fallando

---
+++ TestJitsiApp/App.xaml.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace TestJitsiApp
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+
+            InitializeComponent();
+            VersionTracking.Track();
+            MainPage = new MainPage();
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
